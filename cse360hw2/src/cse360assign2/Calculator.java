2a6d3152367f5f788bf7b93b1bcd9c2e50c3428b
Jason Carlson <jtcarlso@asu.edu>
2019-02-16 22:17:44 -0700

    Added comments.

---
+++ cse360hw2/src/cse360assign2/Calculator.java
@@ -0,0 +1,78 @@
+/**
+ * @author Jason Carlson
+ * @class CSE 360
+ * @instructor Debra Calliss
+ * @assignment #2
+ * @classID 344
+ */
+
+
+
+package cse360assign3;
+
+/**
+ * 
+ * The Calculator class contains basic methods that a calculator would have.
+ * This includes adding, subtracting, dividing.
+ * 
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor for the calculator. Initializes total to 0.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total 
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds a value
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts a value
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies a value
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides by value
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns history
+	 * @return string of history
+	 */
+	public String getHistory () {
+		return "";
+	}
+	
+}
